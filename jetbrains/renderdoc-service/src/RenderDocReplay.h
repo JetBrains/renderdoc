01e8550fb30563e552c37804eb2a38894ed8bfa6
nadezhda.melnikova <nadezhda.melnikova@jetbrains.com>
2024-10-07 17:18:38 +0300

    Added debugPixel option

--- jetbrains/renderdoc-service/src/RenderDocReplay.h
+++ jetbrains/renderdoc-service/src/RenderDocReplay.h
@@ -3,17 +3,20 @@
 #include "RenderDocDebugSession.h"
 #include "RenderDocModel/RdcCapture.Generated.h"
 
+enum class ShaderStage : uint8_t;
 struct IReplayController;
 
 namespace jetbrains::renderdoc {
 
 class RenderDocReplay : public model::RdcCapture {
   std::shared_ptr<IReplayController> controller;
+  [[nodiscard]] rd::Wrapper<RenderDocDebugSession> debug(const ShaderStage &stage, const rd::Lifetime &session_lifetime, uint32_t event_id) const;
 
 public:
   explicit RenderDocReplay(IReplayController *controller);
 
   [[nodiscard]] rd::Wrapper<RenderDocDebugSession> debug_vertex(const rd::Lifetime &session_lifetime, uint32_t event_id) const;
+  [[nodiscard]] rd::Wrapper<RenderDocDebugSession> debug_pixel(const rd::Lifetime &session_lifetime, uint32_t event_id) const;
 };
 
 } // namespace jetbrains::renderdoc
