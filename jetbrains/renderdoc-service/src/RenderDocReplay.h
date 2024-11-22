0ed5552213466e1a97546a40c847aa50cb28ac68
nadezhda.melnikova <nadezhda.melnikova@jetbrains.com>
2024-11-22 13:51:02 +0200

    Pixel selection added

--- jetbrains/renderdoc-service/src/RenderDocReplay.h
+++ jetbrains/renderdoc-service/src/RenderDocReplay.h
@@ -20,12 +20,12 @@ public:
 
   [[nodiscard]] std::vector<rd::Wrapper<model::RdcWindowOutputData>> get_textureRGBBuffer(const rd::Lifetime &session_lifetime, uint32_t event_id) const;
   [[nodiscard]] rd::Wrapper<RenderDocDebugSession> debug_vertex(const rd::Lifetime &session_lifetime, uint32_t event_id) const;
-  [[nodiscard]] rd::Wrapper<RenderDocDebugSession> debug_pixel(const rd::Lifetime &session_lifetime, uint32_t event_id) const;
+  [[nodiscard]] rd::Wrapper<RenderDocDebugSession> debug_pixel(const rd::Lifetime &session_lifetime, const model::RdcDebugPixelInput &input) const;
   [[nodiscard]] rd::Wrapper<RenderDocDebugSession> try_debug_vertex(const rd::Lifetime &session_lifetime, const std::vector<rd::Wrapper<model::RdcSourceBreakpoint>> &breakpoints) const;
-  [[nodiscard]] rd::Wrapper<RenderDocDebugSession> try_debug_pixel(const rd::Lifetime &session_lifetime, const std::vector<rd::Wrapper<model::RdcSourceBreakpoint>> &breakpoints) const;
+  [[nodiscard]] rd::Wrapper<RenderDocDebugSession> try_debug_pixel(const rd::Lifetime &session_lifetime, const model::RdcDebugPixelInput &input) const;
 
   [[nodiscard]] rd::Wrapper<RenderDocDrawCallDebugSession> start_debug_vertex(const ActionDescription * action) const;
-  [[nodiscard]] rd::Wrapper<RenderDocDrawCallDebugSession> start_debug_pixel(const ActionDescription * action) const;
+  [[nodiscard]] rd::Wrapper<RenderDocDrawCallDebugSession> start_debug_pixel(const ActionDescription * action, DebugInput input) const;
 };
 
 } // namespace jetbrains::renderdoc
