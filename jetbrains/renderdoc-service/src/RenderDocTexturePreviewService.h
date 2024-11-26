5acd553460d3d19f39165deed046a6b25570033f
nadezhda.melnikova <nadezhda.melnikova@jetbrains.com>
2024-11-26 12:42:17 +0200

    Outputs calculating on copy/clear/compute fixed

--- jetbrains/renderdoc-service/src/RenderDocTexturePreviewService.h
+++ jetbrains/renderdoc-service/src/RenderDocTexturePreviewService.h
@@ -11,6 +11,7 @@ namespace jetbrains::renderdoc {
 namespace model {
 class RdcWindowOutputData;
 }
+
 class RenderDocTexturePreviewService {
   struct Dimensions {
     int32_t width;
@@ -21,16 +22,21 @@ class RenderDocTexturePreviewService {
   };
 
   IReplayController *controller;
-  std::unordered_map<uint32_t, std::unordered_map<std::size_t, Dimensions>> outputs_dimensions_cache;
+  std::unordered_map<uint32_t, std::vector<std::pair<Descriptor, Dimensions>>> outputs_cache;
   std::unordered_map<uint32_t, Dimensions> max_dimensions;
 
-  void calculate_dimensions(uint32_t event_id, const rdcarray<Descriptor> &outputs);
+  static Descriptor create_descriptor(ResourceId id);
+
+  void calculate_dimensions(const ActionDescription *action);
+  void calculate_action_context(const ActionDescription *action, bool &copy, bool &clear, bool &compute) const;
+  rdcarray<Descriptor> get_output_targets(const ActionDescription *action) const;
+  Descriptor get_depth_target(const ActionDescription *action) const;
   int32_t get_width(uint32_t event_id, std::size_t output_index) const;
   int32_t get_height(uint32_t event_id, std::size_t output_index) const;
 
 public:
   explicit RenderDocTexturePreviewService(IReplayController *controller);
-  std::vector<rd::Wrapper<model::RdcWindowOutputData>> get_buffers(uint32_t event_id, const rdcarray<Descriptor> &outputs);
+  std::vector<rd::Wrapper<model::RdcWindowOutputData>> get_buffers(const ActionDescription *action);
 };
 }
 
